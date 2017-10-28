7f6a5f936cd654f85778c0f98177672c2be7112c
ethankulman <ethan@Ethans-MBP.home>
2017-10-28 11:42:20 -0400

    added track mods

--- app.py
+++ app.py
@@ -22,6 +22,8 @@ sources = [{'source': 'NyTimes', 'url': 'https://www.nytimes.com/section/politic
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 db = SQLAlchemy(app)
 
